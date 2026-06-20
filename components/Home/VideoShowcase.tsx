b4bc6cb78b2a6a4e50f86d9b1bdd754172617dab
Yogesh Pal <yogeshpal5049@gmail.com>
2026-06-20 15:24:26 +0530

    update

--- components/Home/VideoShowcase.tsx
+++ components/Home/VideoShowcase.tsx
@@ -4,7 +4,7 @@ export default function VideoShowcase() {
   return (
     <section className="relative w-full overflow-hidden" style={{ height: "80vh", maxHeight: "600px" }}>
       <video
-        src="https://res.cloudinary.com/dxfkygu6e/video/upload/v1781675697/WhatsApp_Video_2026-06-17_at_11.19.56_AM_ghjlk4.mp4"
+        src="https://res.cloudinary.com/dxfkygu6e/video/upload/v1781948533/WhatsApp_Video_2026-06-19_at_6.07.58_PM_jj6ezx.mp4"
         autoPlay
         muted
         loop
