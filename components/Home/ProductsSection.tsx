adab033827310b6762f953634b37a685d1aed2bf
Yogesh Pal <yogeshpal5049@gmail.com>
2026-06-28 20:54:19 +0530

    done

--- components/Home/ProductsSection.tsx
+++ components/Home/ProductsSection.tsx
@@ -70,7 +70,7 @@ export default function ProductsSection() {
         </motion.div>
 
         {/* Product cards grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-3">
           {PRODUCTS.map((product, i) => (
             <motion.div
               key={product.title}
